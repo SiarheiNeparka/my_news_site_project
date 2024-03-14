d90ea24ce18ff06646aff3afd32441036f3b9912
Siarhei Neparka <fume838@gmail.com>
2024-03-14 20:57:28 +0300

    creating django project (mywebsite), 2 django apps (hello and news) and design models.py for news app

---
+++ mywebsite/news/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+from django.utils import timezone
+
+
+# Create your models here.
+class Reporter(models.Model):
+    full_name = models.CharField(max_length=70)
+
+    def __str__(self):
+        return self.full_name
+
+
+class Article(models.Model):
+    publish = models.DateField(default=timezone.now)
+    headline = models.CharField(max_length=200)
+    content = models.TextField()
+    reporter = models.ForeignKey(Reporter, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.headline

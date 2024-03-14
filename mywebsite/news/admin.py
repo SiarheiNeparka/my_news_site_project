58f99cb4274d2e7f0c995e19844eeffc657f08e2
Siarhei Neparka <fume838@gmail.com>
2024-03-14 21:19:08 +0300

    creating a superuser (spam) and changing admin.py for news app (add model and customize admin site)

--- mywebsite/news/admin.py
+++ mywebsite/news/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Article
+
 
 # Register your models here.
+# admin.site.register(Article)
+
+
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ["headline", "reporter", "publish"]
+    list_filter = ["publish", "reporter"]
+    search_fields = ["headline", "content"]
+    raw_id_fields = ["reporter"]
+    date_hierarchy = "publish"
+    ordering = ["publish"]

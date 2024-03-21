8577998c7eeea39b38f51b1af11f15aeeee5144e
Siarhei Neparka <139542921+SiarheiNeparka@users.noreply.github.com>
2024-03-21 21:29:05 +0300

    Update admin.py

--- mywebsite/news/admin.py
+++ mywebsite/news/admin.py
@@ -2,10 +2,6 @@ from django.contrib import admin
 from .models import Article
 
 
-# Register your models here.
-# admin.site.register(Article)
-
-
 @admin.register(Article)
 class ArticleAdmin(admin.ModelAdmin):
     list_display = ["headline", "reporter", "publish"]

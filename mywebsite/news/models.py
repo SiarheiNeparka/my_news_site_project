3187cb6d0d9d9593abf44c09701505f6aebffa66
Siarhei Neparka <fume838@gmail.com>
2024-03-26 19:07:57 +0300

    changing the models.py file (addig new SlugField to Article)

--- mywebsite/news/models.py
+++ mywebsite/news/models.py
@@ -9,6 +9,7 @@ class Article(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     headline = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=250)
     content = models.TextField()
     reporter = models.ForeignKey(
         User,

901c1df49c286d6f228b1b6d52dcd6ada60eb8a1
Siarhei Neparka <fume838@gmail.com>
2024-03-26 21:27:09 +0300

    changing news urls.py file (change post to article_slg and add / to the end)

--- mywebsite/news/urls.py
+++ mywebsite/news/urls.py
@@ -7,7 +7,7 @@ app_name = 'news'
 urlpatterns = [
     path('', views.article_list, name='article_list'),
     path(
-        '<int:year>/<int:month>/<int:day>/<slug:post>',
+        '<int:year>/<int:month>/<int:day>/<slug:article_slg>/',
         views.article_detail,
         name='article_detail',
     ),

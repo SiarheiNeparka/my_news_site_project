fa990d18196d8b695ed2a6873d722f0897b666b0
Siarhei Neparka <fume838@gmail.com>
2024-03-26 19:06:11 +0300

    changing the models.py file (addig new fields to Article)

--- mywebsite/news/models.py
+++ mywebsite/news/models.py
@@ -6,6 +6,8 @@ from django.contrib.auth.models import User
 # Create your models here.
 class Article(models.Model):
     publish = models.DateField(default=timezone.now)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
     headline = models.CharField(max_length=200)
     content = models.TextField()
     reporter = models.ForeignKey(

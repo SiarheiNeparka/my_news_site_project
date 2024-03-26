9f3cd3355f5923b7bad5cc71c6cae7dfbefa7449
Siarhei Neparka <fume838@gmail.com>
2024-03-26 20:41:35 +0300

    change models.py file (add get_absolite_url function)

--- mywebsite/news/models.py
+++ mywebsite/news/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 
 # Create your models here.
@@ -40,6 +41,9 @@ class Article(models.Model):
         indexes = [
             models.Index(fields=["-publish"]),
         ]
+
+    def get_absolute_url(self):
+        return reverse("news:article_detail", args=[self.id])
 
     def __str__(self):
         return self.headline

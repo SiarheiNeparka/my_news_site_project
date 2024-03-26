4eb56a329656ff81bfa4afc5eba3b1efa9c266e2
Siarhei Neparka <fume838@gmail.com>
2024-03-26 19:46:02 +0300

    changing models.py file (add new class PublishManager)

--- mywebsite/news/models.py
+++ mywebsite/news/models.py
@@ -4,6 +4,11 @@ from django.contrib.auth.models import User
 
 
 # Create your models here.
+class PublishedManager(models.Manager):
+    def get_queryset(self) -> models.QuerySet:
+        return super().get_queryset().filter(status=Article.Status.PUBLISHED)
+
+
 class Article(models.Model):
     publish = models.DateTimeField(default=timezone.now)
     created = models.DateTimeField(auto_now_add=True)

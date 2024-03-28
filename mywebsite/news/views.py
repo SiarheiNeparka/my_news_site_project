0d8d9dce8bc9283a8788022286f7edcc188c1e61
Siarhei Neparka <fume838@gmail.com>
2024-03-28 20:28:06 +0300

    change views.py file (replace article_list func by ArticleListView class)

--- mywebsite/news/views.py
+++ mywebsite/news/views.py
@@ -1,22 +1,17 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Article
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.views.generic.list import ListView
 
 
 # Create your views here.
-def article_list(request):
-    articles = Article.published.all()
+class ArticleListView(ListView):
+    '''Alternative article list view.'''
 
-    paginator = Paginator(articles, 2)
-    page_number = request.GET.get('page', 1)
-    try:
-        articles = paginator.page(page_number)
-    except EmptyPage:
-        articles = paginator.page(paginator.num_pages)
-    except PageNotAnInteger:
-        articles = paginator.page(1)
-
-    return render(request, 'news/article/list.html', {'articles': articles})
+    queryset = Article.published.all()
+    context_object_name = 'articles'
+    paginate_by = 2
+    template_name = 'news/article/list.html'
 
 
 def article_detail(request, year, month, day, article_slg):

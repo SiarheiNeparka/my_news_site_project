5e39417bb5e36fb0df79f693f5f7387e5695e6f4
Siarhei Neparka <fume838@gmail.com>
2024-03-28 20:18:54 +0300

    change views.py (fixing EmptyPage error)

--- mywebsite/news/views.py
+++ mywebsite/news/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Article
-from django.core.paginator import Paginator
+from django.core.paginator import Paginator, EmptyPage
 
 
 # Create your views here.
@@ -9,7 +9,10 @@ def article_list(request):
 
     paginator = Paginator(articles, 2)
     page_number = request.GET.get('page', 1)
-    articles = paginator.page(page_number)
+    try:
+        articles = paginator.page(page_number)
+    except EmptyPage:
+        articles = paginator.page(paginator.num_pages)
 
     return render(request, 'news/article/list.html', {'articles': articles})
 

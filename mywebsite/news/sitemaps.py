fbce34cfd45e5e062d95e370903805eb8be2bdee
Siarhei Neparka <fume838@gmail.com>
2024-04-11 13:52:01 +0300

    change a sitemaps.py file (some more fixes)

--- mywebsite/news/sitemaps.py
+++ mywebsite/news/sitemaps.py
@@ -7,7 +7,7 @@ class ArticleSitemap(Sitemap):
     priority = 0.9
 
     def items(self):
-        return Article.publish.all()
+        return Article.published.all()
 
     def lastmod(self, obj):
         return obj.updated
